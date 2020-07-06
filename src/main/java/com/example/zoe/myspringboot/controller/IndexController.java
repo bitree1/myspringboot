f4057f4d2680e50d9af0d99c93c6c1b4fa523bb7
zoe <1164739892@qq.com>
2020-07-06 14:41:32 +0800

    引入新jar

--- src/main/java/com/example/zoe/myspringboot/controller/IndexController.java
+++ src/main/java/com/example/zoe/myspringboot/controller/IndexController.java
@@ -1,11 +1,15 @@
 package com.example.zoe.myspringboot.controller;
 
+import com.example.zoe.myspringboot.utils.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 
 @RestController
 public class IndexController {
@@ -20,6 +24,7 @@ public class IndexController {
     @GetMapping("/index")
     public String Index1() {
         log.info("HELLO index!");
-        return "HELLO index";
+        String Date = DateUtils.DateFormtString(new Date());
+        return Date;
     }
 }
